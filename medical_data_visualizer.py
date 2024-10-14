0c938962976f662d7e39e864e01a9793cd618d86
Berlyn Mutinda <berlynmumbua@gmail.com>
2024-10-14 11:21:40 +0000

    Medical data project

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,40 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = df['weight'] / (df['height'] / 100) ** 2
 
 # 3
 
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1) 
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value'])['value'].count().reset_index(name='count')
+
+    sns.catplot(x='variable', y='count', hue='value', col='cardio', data=df_cat, kind='bar')
+
+    plt.show()
 
     # 7
+    df_long = df.melt(id_vars='cardio', var_name='feature', value_name='value')
 
+    df_counts = df_long.groupby(['cardio', 'feature', 'value'])['value'].count().reset_index(name='count')
+
+    sns.catplot(x='feature', y='count', hue='value', col='cardio', data=df_counts, kind='bar')
+
+    plt.show()
 
 
     # 8
-    fig = None
+    fig = plt.gcf()
 
 
     # 9
@@ -38,21 +48,24 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[(df['ap_lo'] <= df['ap_hi']) &
+                 (df['height'] >= df['height'].quantile(0.025)) &
+                 (df['height'] <= df['height'].quantile(0.975)) &
+                 (df['weight'] >= df['weight'].quantile(0.025)) &
+                 (df['weight'] <= df['weight'].quantile(0.975))]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
 
     # 14
-    fig, ax = None
+    fig, ax =plt.subplots(figsize=(10, 8))
 
     # 15
-
+    sns.heatmap(corr, annot=True, cmap='coolwarm', mask=mask)
 
 
     # 16
